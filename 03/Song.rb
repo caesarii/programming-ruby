a3f362a5c1221cadc00ba4977afa12ea31189596
青禾 <qinghe@qunhemail.com>
2018-08-18 10:27:25 +0800

    feat: 重载 to_s

--- 03/Song.rb
+++ 03/Song.rb
@@ -4,6 +4,9 @@ class Song
         @artist = artist
         @duration = duration
     end
+    def to_s
+        return "Song: #@name--#@artist (#@duration)"
+    end
 end
 
 song = Song.new('爱的劳工', '左小祖咒', 260)
