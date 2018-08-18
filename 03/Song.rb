dede19f3e0e5176e6383b17bec4a74c6c975fac5
青禾 <qinghe@qunhemail.com>
2018-08-18 11:35:26 +0800

    feat: 类属性

--- 03/Song.rb
+++ 03/Song.rb
@@ -1,8 +1,10 @@
 class Song
+    @@plays = 0
     def initialize(name, artist, duration)
         @name = name
         @artist = artist
         @duration = duration
+        @plays = 0
     end
     attr_reader :name, :artist, :duration
     attr_writer :duration
@@ -15,8 +17,12 @@ class Song
     def to_s
         return "Song: #@name--#@artist (#@duration)"
     end
+    def play
+        @plays += 1
+        @@plays += 1
+        return "This song: #@plays plays. Total #@@plays plays"
+    end
 end
-
 song = Song.new('爱的劳工', '左小祖咒', 260)
 id = song.inspect
 puts(id)
@@ -34,3 +40,9 @@ puts(song.duration)
 puts(song.duration_in_minutes)
 song.duration_in_minutes = 5
 puts(song.duration_in_minutes)
+
+song2 = Song.new('陈升', '把悲伤留给自己', 360)
+song2.play()
+puts(song.play())
+puts(song.play())
+
