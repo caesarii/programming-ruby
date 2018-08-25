abf116aae2e8488b8b8be99d7aa4f0d983eee6dd
caesarii <iwangqinghe@gmail.com>
2018-08-25 15:43:54 +0800

    file.open 两种调用

--- 04/file.rb
+++ 04/file.rb
@@ -1,9 +1,14 @@
 class File
     # 该方法负责文件的打开和关闭, 处理通过 block 实现
     def File.open_and_process(*args)
-        f = File.open(*args)
-        yield f
-        f.close()
+        # 普通调用返回文件对象
+        result = file = File.new(*args)
+        # block 调用则传递到 block
+        if block_given?()
+            result = yield file
+            file.close
+        end
+        return result
     end
 end
 
@@ -12,4 +17,7 @@ File.open_and_process('testfile', 'r') do |file|
         puts line
     end
 end
+
+file = File.open_and_process('testfile', 'r')
+puts(file)
 
